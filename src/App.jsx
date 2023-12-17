8a2c5a24f8be9803b87152ba7795f25992d2a424
thisissikandar <sikandarc989@gmail.com>
2023-12-17 11:57:14 +0530

    redux Toolkit Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="bg-slate-700 text-white h-screen">App</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-export default App
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-500 text-white">
+    
+    <div className="w-full block">
+      <Header/>
+      <main>
+        {/* <Outlet/> */} Too
+      </main>
+      <Footer/>
+    </div>
+    </div>
+  ) : null;
+};
+
+export default App;
